ada55cdcaf45a2701580c0b4471738bcd97beec1
haerihaeri <goflll@naver.com>
2024-06-11 11:12:30 +0900

    add transformer

---
+++ model/transformer/transformer_haeri/models/blocks/encoder_layer.py
@@ -0,0 +1,41 @@
+"""
+@author : Hyunwoong
+@when : 2019-10-24
+@homepage : https://github.com/gusdnd852
+"""
+from torch import nn
+
+from models.layers.layer_norm import LayerNorm
+from models.layers.multi_head_attention import MultiHeadAttention
+from models.layers.position_wise_feed_forward import PositionwiseFeedForward
+
+
+class EncoderLayer(nn.Module): # 하나의 인코더 레이어는 멀티헤드어텐션->드롭아웃->normalization->ffn->dropout->normalization으로 나옴
+
+    def __init__(self, d_model, ffn_hidden, n_head, drop_prob):
+        super(EncoderLayer, self).__init__()
+        self.attention = MultiHeadAttention(d_model=d_model, n_head=n_head)
+        self.dropout1 = nn.Dropout(p=drop_prob) 
+        self.norm1 = LayerNorm(d_model=d_model) # 여기에도 학습 가능 파라미터가 있나?
+
+        self.ffn = PositionwiseFeedForward(d_model=d_model, hidden=ffn_hidden, drop_prob=drop_prob)
+        self.dropout2 = nn.Dropout(p=drop_prob)
+        self.norm2 = LayerNorm(d_model=d_model)
+
+    def forward(self, x, src_mask):
+        # 1. compute self attention
+        _x = x
+        x = self.attention(q=x, k=x, v=x, mask=src_mask)
+        
+        # 2. add and norm
+        x = self.dropout1(x)
+        x = self.norm1(x + _x) # x+_x 를 인풋으로 넣어서, residual connection 추가
+        
+        # 3. positionwise feed forward network
+        _x = x
+        x = self.ffn(x)
+      
+        # 4. add and norm
+        x = self.dropout2(x)
+        x = self.norm2(x + _x)
+        return x
